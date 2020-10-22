31b5b9f33983777c2686a93b7689652ab94bf43b
Info2 <nicolll.chazatar@udea.edu.co>
2020-10-22 08:57:23 -0500

    Inicializacion variable que almacena la cadena de entrada e impresion caracter a caracter

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+/*
+ */
+
 #include <iostream>
+//Variables globales y marcos
+#define TAM (int)10
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+   // char a[]={0};
+    char a[]="AB11CD44";
+    for(int var=0; var<TAM;++var){
+        cout<<a[var]<<endl;
+    }
     return 0;
 }
